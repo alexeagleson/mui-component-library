630df0a4dc2a0b9b440b4bea131139c22cdfd598
Alex Eagleson <alexeagleson@gmail.com>
2022-04-26 18:27:45 -0400

    feat: add mui icon to example component

--- src/components/buttons/custom-button/MyCustomButton.tsx
+++ src/components/buttons/custom-button/MyCustomButton.tsx
@@ -1,9 +1,14 @@
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import Button from '@mui/material/Button';
 
 export interface IMyCustomButtonProps {}
 
 const MyCustomButton: React.FC<IMyCustomButtonProps> = () => {
-  return <Button sx={{ color: 'secondary.main' }}>Custom Button</Button>;
+  return (
+    <Button sx={{ color: 'secondary.main' }} startIcon={<CheckBoxIcon />}>
+      Custom Button
+    </Button>
+  );
 };
 
 export default MyCustomButton;
