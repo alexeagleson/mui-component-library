ef14e0148844c853b4cabdde16be518f4daa4831
Alex Eagleson <alexeagleson@gmail.com>
2022-04-28 10:27:21 -0400

    build: make public

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: ['plugin:react/recommended', 'plugin:storybook/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
   },
   globals: {
-    react: 'readonly'
-  }
+    react: 'readonly',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
