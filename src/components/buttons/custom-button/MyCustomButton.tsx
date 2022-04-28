78c5f9bf9f1940fec3391673415c255ec7f6d23b
Alex Eagleson <alexeagleson@gmail.com>
2022-04-28 10:23:56 -0400

    refactor: remove icon dependency

--- src/components/buttons/custom-button/MyCustomButton.tsx
+++ src/components/buttons/custom-button/MyCustomButton.tsx
@@ -1,14 +1,9 @@
-import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import Button from '@mui/material/Button';
 
 export interface IMyCustomButtonProps {}
 
 const MyCustomButton: React.FC<IMyCustomButtonProps> = () => {
-  return (
-    <Button sx={{ color: 'secondary.main' }} startIcon={<CheckBoxIcon />}>
-      Custom Button
-    </Button>
-  );
+  return <Button sx={{ color: 'secondary.main' }}>Custom Button</Button>;
 };
 
 export default MyCustomButton;
